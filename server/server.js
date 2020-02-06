39bcd5ed276d870b4c1f15284de32818bf03ed88
Perumal <perumalsrinu8@gmail.com>
2020-02-06 15:45:26 +0530

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express()
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// GET - Retrieve the data from the server
+app.get('/', (req, res) => {
+    res.json("Hello World")
+});
+
+// POST - Send the data from frontend to backend 
+app.post('/', (req, res) => {
+    console.log(req.body);
+});
+
+
+app.listen(3000, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Listening on Port", 3000);
+    }
+});
