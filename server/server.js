54b7bbecb68546e304a88294904ab2e52d6f5414
Perumal <perumalsrinu8@gmail.com>
2020-02-06 17:46:21 +0530

    website

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 
 const app = express()
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connecting to the Database");
+    }
+})
 
 // GET - Retrieve the data from the server
 app.get('/', (req, res) => {
